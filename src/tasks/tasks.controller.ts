8ba4411bdbb91988b4d8e6f8b26d8917fa12f29e
Snipsx17 <ing.uberthhernandez@gmail.com>
2025-09-29 12:59:42 -0400

    Feat: update tasks endpoints

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,5 @@
 import {
+  Body,
   Controller,
   Delete,
   Get,
@@ -8,40 +9,53 @@ import {
   Patch,
   Post,
   Put,
+  Query,
 } from '@nestjs/common';
 import { ParseUuidV4Pipe } from 'src/common/pipes/parse-uuidv4.pipe';
+import { TasksService } from './tasks.service';
+import { CreateTaskDto } from './dtos/create-task.dto';
+import { UpdateTaskDto } from './dtos/update-task.dto';
+import { ParseTaskStatusPipe } from 'src/common/pipes/parse-task-status.pipe';
 
 @Controller('tasks')
 export class TasksController {
+  constructor(private readonly tasksService: TasksService) {}
+
   @Get()
-  getTasks() {
-    return 'This will return all tasks';
+  async getTasks() {
+    return await this.tasksService.getAll();
   }
 
   @Get(':id')
-  getTasksById(@Param('id', ParseUuidV4Pipe) id: string) {
-    return 'This will return a task by ID';
+  async getTasksById(@Param('id', ParseUuidV4Pipe) id: string) {
+    return await this.tasksService.getById(id);
   }
 
   @HttpCode(HttpStatus.CREATED)
   @Post()
-  createTask() {
-    return 'This will create a new task';
+  async createTask(@Body() createTaskDto: CreateTaskDto) {
+    return await this.tasksService.create(createTaskDto);
   }
 
   @Put(':id')
-  updateTask(@Param('id', ParseUuidV4Pipe) id: string) {
-    return 'This will update a task';
+  async updateTask(
+    @Param('id', ParseUuidV4Pipe) id: string,
+    @Body() updatedTaskDto: UpdateTaskDto,
+  ) {
+    return await this.tasksService.update(id, updatedTaskDto);
   }
 
   @HttpCode(HttpStatus.NO_CONTENT)
   @Delete(':id')
-  deleteTask(@Param('id', ParseUuidV4Pipe) id: string) {
-    return 'This will delete a task';
+  async deleteTask(@Param('id', ParseUuidV4Pipe) id: string) {
+    return await this.tasksService.delete(id);
   }
 
   @Patch(':id/status')
-  updateTaskStatus(@Param('id', ParseUuidV4Pipe) id: string) {
-    return 'This will update a task status';
+  async updateTaskStatus(
+    @Param('id', ParseUuidV4Pipe) id: string,
+    @Query('new-status', ParseTaskStatusPipe) newStatus: string,
+  ) {
+    return await this.tasksService.updateStatus(id, newStatus);
   }
 }
