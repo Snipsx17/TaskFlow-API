65992cfb412537fc9f19b226a442986639240bc6
Snipsx17 <ing.uberthhernandez@gmail.com>
2025-10-09 06:29:30 -0400

    Feat: implements typeorm

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,40 +1,91 @@
-import { Injectable } from '@nestjs/common';
-import { TasksRepository } from './repositories/tasks.repository';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateTaskDto } from './dtos/create-task.dto';
-import { v4 as UUID } from 'uuid';
 import { UpdateTaskDto } from './dtos/update-task.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { TaskEntity } from './entities/task.entity';
+import { EntityNotFoundError, Repository } from 'typeorm';
+import { TaskStatus } from './enums/task-status.enum';
 
 @Injectable()
 export class TasksService {
-  constructor(private readonly tasksRepository: TasksRepository) {}
+  constructor(
+    @InjectRepository(TaskEntity)
+    private readonly taskRepository: Repository<TaskEntity>,
+  ) {}
 
   async getAll() {
-    return await this.tasksRepository.getAll();
+    return await this.taskRepository.find();
   }
 
   async getById(taskId: string) {
-    return await this.tasksRepository.getById(taskId);
+    try {
+      const task = await this.getTaskFromDB(taskId);
+
+      return task;
+    } catch (error) {
+      this.handleDBExceptions(error);
+    }
   }
 
   async create(createTaskDto: CreateTaskDto) {
-    const task = {
-      id: UUID(),
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      ...createTaskDto,
-    };
-    return await this.tasksRepository.create(task);
+    try {
+      const newTask = this.taskRepository.create(createTaskDto);
+      await this.taskRepository.save(newTask);
+      return newTask;
+    } catch (error) {
+      this.handleDBExceptions(error);
+    }
   }
 
   async update(taskId: string, updatedTaskDto: UpdateTaskDto) {
-    return await this.tasksRepository.update(taskId, updatedTaskDto);
+    try {
+      await this.getTaskFromDB(taskId);
+      await this.taskRepository.update(taskId, updatedTaskDto);
+      return this.taskRepository.findBy({ id: taskId });
+    } catch (error) {
+      this.handleDBExceptions(error);
+    }
   }
 
   async delete(taskId: string) {
-    return await this.tasksRepository.delete(taskId);
+    try {
+      await this.getTaskFromDB(taskId);
+      return await this.taskRepository.delete(taskId);
+    } catch (error) {
+      this.handleDBExceptions(error);
+    }
   }
 
-  async updateStatus(taskId: string, newStatus: string) {
-    return await this.tasksRepository.updateStatus(taskId, newStatus);
+  async updateStatus(taskId: string, newStatus: TaskStatus) {
+    try {
+      const task = await this.getTaskFromDB(taskId);
+
+      task[0].status = newStatus;
+      await this.taskRepository.save(task[0]);
+
+      return task;
+    } catch (error) {
+      this.handleDBExceptions(error);
+    }
+  }
+
+  async getTaskFromDB(taskId: string) {
+    const task = await this.taskRepository.findOneByOrFail({ id: taskId });
+
+    return task;
+  }
+
+  private handleDBExceptions(error: any): never {
+    if (error.code === '23505') throw new BadRequestException(error.detail);
+
+    if (error instanceof EntityNotFoundError)
+      throw new NotFoundException('Task not found');
+
+    throw new InternalServerErrorException();
   }
 }
