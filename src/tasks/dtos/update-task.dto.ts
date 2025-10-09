995818bcdaa0b6cf50ac1470519cf9e521cc3989
Snipsx17 <ing.uberthhernandez@gmail.com>
2025-10-09 06:27:44 -0400

    update: make all properties optionals in update task dto

--- src/tasks/dtos/update-task.dto.ts
+++ src/tasks/dtos/update-task.dto.ts
@@ -1,6 +1,7 @@
 import {
   IsDateString,
   IsEnum,
+  IsOptional,
   IsString,
   IsUUID,
   Length,
@@ -11,21 +12,27 @@ import { TaskPriority } from '../enums/task-priority.enum';
 export class UpdateTaskDto {
   @IsString()
   @Length(3, 50)
-  readonly title: string;
+  @IsOptional()
+  readonly title?: string;
 
   @IsString()
   @Length(3, 255)
+  @IsOptional()
   readonly description: string;
 
   @IsEnum(TaskStatus)
+  @IsOptional()
   readonly status: TaskStatus;
 
   @IsEnum(TaskPriority)
+  @IsOptional()
   readonly priority: TaskPriority;
 
   @IsDateString()
+  @IsOptional()
   readonly dueDate: Date;
 
   @IsUUID('4')
+  @IsOptional()
   readonly categoryId: string;
 }
