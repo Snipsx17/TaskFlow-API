cb883df4ac963d8d06eac35393c2e734951b1669
Uberth Hernandez <ing.uberthhernandez@gmail.com>
2025-10-27 15:51:05 -0400

    Update: use partialType to create update task dto

--- src/tasks/dtos/update-task.dto.ts
+++ src/tasks/dtos/update-task.dto.ts
@@ -1,44 +1,4 @@
-import {
-  IsArray,
-  IsDateString,
-  IsEnum,
-  IsOptional,
-  IsString,
-  IsUUID,
-  Length,
-} from 'class-validator';
-import { TaskStatus } from '../enums/task-status.enum';
-import { TaskPriority } from '../enums/task-priority.enum';
+import { CreateTaskDto } from './create-task.dto';
+import { PartialType } from '@nestjs/mapped-types';
 
-export class UpdateTaskDto {
-  @IsString()
-  @Length(3, 50)
-  @IsOptional()
-  readonly title?: string;
-
-  @IsString()
-  @Length(3, 255)
-  @IsOptional()
-  readonly description?: string;
-
-  @IsEnum(TaskStatus)
-  @IsOptional()
-  readonly status?: TaskStatus;
-
-  @IsEnum(TaskPriority)
-  @IsOptional()
-  readonly priority?: TaskPriority;
-
-  @IsDateString()
-  @IsOptional()
-  readonly dueDate?: Date;
-
-  @IsUUID('4')
-  @IsOptional()
-  readonly categoryId?: string;
-
-  @IsArray()
-  @IsString({ each: true })
-  @IsOptional()
-  tags?: string[];
-}
+export class UpdateTaskDto extends PartialType(CreateTaskDto) {}
